28707ee51f474d7c538d0a674c373392829c123b
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-11-17 00:26:40 +0300

    initial commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Abdullah Oğuz on 15.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
